6444d12b93d4c0e7975305aaf7260bdafd66bb0a
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-01-17 16:07:24 -0500

    ajout de la météo à long terme

--- app/components/MeteoVille.js
+++ app/components/MeteoVille.js
@@ -5,9 +5,13 @@ const MeteoVille = (props) => (props.enChargement === true
     : <div>
         <h2>Météo de {props.meteo.nom}</h2>
         <div>
-            <p>Temperature: {props.meteo.temp.courant}</p>
-            <p>Maximum: {props.meteo.temp.max}</p>
-            <p>Minimum: {props.meteo.temp.min}</p>
+            <p>Matin: {props.meteo.temperature["0"].temp.matin}</p>
+            <p>Jour: {props.meteo.temperature["0"].temp.jour}</p>
+            <p>Soir: {props.meteo.temperature["0"].temp.soir}</p>
+            <p>Nuit: {props.meteo.temperature["0"].temp.nuit}</p>
+            <p>Maximum: {props.meteo.temperature["0"].temp.max}</p>
+            <p>Minimum: {props.meteo.temperature["0"].temp.min}</p>
+            <p>Condition: {props.meteo.temperature["0"].condition}</p>
         </div>
     </div>);
 
--- app/utils/meteoHelper.js
+++ app/utils/meteoHelper.js
@@ -1,42 +1,74 @@
 // pour récupérer les donnés de l'API de météo
 import axios from "axios";
 
-const API_KEY = "&type=accurate&APPID=3004b18e0fdd3386ef62b75d5b459b02";
-const API_URL = "http://api.openweathermap.org/data/2.5/weather?q="
-// http://api.openweathermap.org/data/2.5/weather?q=CITY-NAME&type=accurate&APPID=YOUR-API-KEY
+const API_KEY = "&type=accurate&APPID=3004b18e0fdd3386ef62b75d5b459b02&cnt=5";
+const API_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?q="
 
-const round = (value, precision) => {
-    var multiplier = Math.pow(10, precision || 0);
-    return Math.round(value * multiplier) / multiplier;
+//arrondir un nombre(valeur) selon le nombre de chiffre à la virgule (nbFloat)
+const round = (valeur, nbFloat) => {
+    var multiplicateur = Math.pow(10, nbFloat || 0);
+    return Math.round(valeur * multiplicateur) / multiplicateur;
 }
 
+//convertir de kelvin vers celcius
 const DegreeVersCelcius = (temp) => {
     return round((parseFloat(temp) - 273.15), 1).toFixed(1) + " °C";
 }
 
-const creerObjetMeteo = (meteo) => {
-    let temperature = [meteo.main.temp, meteo.main.temp_max, meteo.main.temp_min].map((x) => {
-        return DegreeVersCelcius(x);
-    })
-    return {
-        nom: meteo.name,
-        temp: {
-            courant: temperature[0],
-            max: temperature[1],
-            min: temperature[2]
-        },
-        condition: meteo.weather["0"].main
-    }
+//pour utiliser map sur un object
+Object.map = (o, f, ctx) => {
+    ctx = ctx || this;
+    var result = {};
+    Object.keys(o).forEach(function(k) {
+        result[k] = f.call(ctx, o[k], k, o);
+    });
+    return result;
 }
 
+//récupérer la température d'un jour
+const RecupererTemp = ({day, max, min, eve, morn, night}) => {
+
+    let temp = {
+        jour: day,
+        max: max,
+        min: min,
+        soir: eve,
+        matin: morn,
+        nuit: night
+    }
+
+    return Object.map(temp, (v,k,o) => {
+        return DegreeVersCelcius(v);
+    });
+}
+
+//créer un objet qui contient la météo de 5 jours
+const creerMeteoCinqJour = (meteo,i) =>{
+    let temperatures = RecupererTemp(meteo.temp);
+
+    return {
+        temp: temperatures,
+        condition: meteo.weather["0"].description,
+    }
+
+}
+
+//obtenir les info meteos d'une ville
 const fetchMeteoInfo = (ville) => {
     return axios.get(API_URL + ville + API_KEY);
 }
 
+//fonction pour récupérer un objet contenant les infos météorologiques d'une ville avec un API
 const meteoHelpers = {
     getMeteoInfo: (ville) => {
         return fetchMeteoInfo(ville).then((meteo) => {
-            return creerObjetMeteo(meteo.data);
+            let meteoCinqJour = meteo.data.list.map((meteo) =>{
+                return creerMeteoCinqJour(meteo);
+            })
+            return {
+                nom: meteo.data.city.name,
+                temperature :meteoCinqJour
+            }
         }).catch(function(err) {
             console.warn('Erreur dans getMeteoInfo: ', err)
         })
